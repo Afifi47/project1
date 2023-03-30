131e5f3dd99f3fc39f5e761b7c4dbcffe98fb4d9
Afifi47 <afifimercurial@gmail.com>
2023-03-30 16:01:52 +0800

    quiz1.js

---
+++ quiz1.js
@@ -0,0 +1,29 @@
+let str = "BENR"
+let num = 3523
+
+let course = str + num
+message = 123
+console.log(course, typeof message)
+
+let data = ["BENR", 3523, true]
+
+console.log(data[0])
+console.log(data[1])
+console.log(data[2])
+console.log(data[3])
+
+let user = {
+    name: "Afifi",
+    age: 22,
+    faculty: "FKEKK",
+}
+console.log(user.name)
+console.log(user.age)
+console.log(user.faculty)
+
+let messages = [
+    {name: "usr1",date: "2/4/23",msg: "mantul",like: 4500},
+    {name: "usr2",date: "5/4/23",msg: "paling mantul",like: 5000}
+]
+console.log(messages[0].msg)
+console.log(messages[1].msg)
